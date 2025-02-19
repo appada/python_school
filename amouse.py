a16a930fd86f708f4331f9615310243e0b1e4c08
CaSaPaPa <129910854+appada@users.noreply.github.com>
2025-02-19 12:31:44 +0900

    Update amouse.py

--- amouse.py
+++ amouse.py
@@ -1,8 +1,20 @@
 import flet as ft
 import time
 import pyautogui
+import keyboard
 
 def main(page: ft.Page):
+
+    def on_key(event: ft.KeyboardEvent):
+        if event.key == "q":
+            print('Q pressed')
+        elif event.key == "Arrow Up":
+            print('Up pressed')
+            page.window.close()
+        page.update()
+
+    page.on_keyboard_event = on_key
+
     count = 0  # 초기 카운트 값
 
     def increment_text(e):
@@ -21,21 +33,29 @@ def main(page: ft.Page):
         clicks = int(clicks_field.value)
         page.update()
 
-        for _ in range(clicks):
+        for index in range(clicks):
+            if keyboard.is_pressed('q'):
+                print('Auto click stopped by user.')
+                break
             pyautogui.click(x, y)
             time.sleep(interval)
 
 
-    X = ft.TextField(label="X", width=10)
+    X = ft.TextField(label="X", width=100)
     Y = ft.TextField(label="Y", width=100)
     interval_field = ft.TextField(label="Interval (seconds)", width=150)
     clicks_field = ft.TextField(label="Number of Clicks", width=150)
     start_button = ft.ElevatedButton(text="Start Auto Click", on_click=start_auto_click)
+    stop_text = ft.Text('Stop Button is Q')
 
-    r_menu = ft.Column([X, Y, interval_field,clicks_field,start_button])
+    r_menu = ft.Column([X, Y, interval_field,clicks_field,start_button,stop_text])
 
     leftright = ft.Row([ numbutton, r_menu])
     page.add(leftright)
+
+    #
+
+    page.update()
 
 if __name__ == "__main__":
 
